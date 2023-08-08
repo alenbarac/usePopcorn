3a835666da6379d53a26bba8db97c83775a76569
Alen <info@alenbarac.com>
2023-08-09 00:02:44 +0200

    layout composition for prop drilling

---
+++ src/StarRating.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react'
+import Star from './Star'
+
+import PropTypes from 'prop-types'
+
+const containerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '16px',
+}
+
+const starContainerStyle = {
+  display: 'flex',
+}
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetStarRating: PropTypes.func,
+}
+
+const StarRating = ({
+  maxRating = 5,
+  color = '#fcc419',
+  size = 48,
+  className = '',
+  messages = [],
+  defaultRating = 0,
+  onSetStarRating,
+}) => {
+  const [rating, setRating] = useState(defaultRating)
+  const [tempRating, setTempRating] = useState(0)
+
+  const textStyle = {
+    lineHeight: '1',
+    margin: '0',
+    color,
+    fontSize: `${size / 1.5}px`,
+  }
+
+  function handleRating(rating) {
+    setRating(rating)
+    onSetStarRating(rating)
+  }
+
+  function handleHoverIn(rating) {
+    setTempRating(rating)
+  }
+
+  function handleHoverOut(rating) {
+    setTempRating(rating)
+  }
+  return (
+    <div style={containerStyle} className={className}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <Star
+            key={i}
+            onRate={() => {
+              handleRating(i + 1)
+            }}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => {
+              handleHoverIn(i + 1)
+            }}
+            onHoverOut={() => {
+              handleHoverOut(0)
+            }}
+            color={color}
+            size={size}
+          />
+        ))}
+      </div>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ''}
+      </p>
+    </div>
+  )
+}
+
+export default StarRating
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+/* import App from './App.jsx'
+import './index.css' */
+
+import StarRating from './StarRating'
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0)
+
+  return (
+    <div>
+      <StarRating size={24} className="test" defaultRating={3} onSetStarRating={setMovieRating} />
+      <p>This movie was rated {movieRating}</p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/*  <App /> */}
+    <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Good', 'Okay', 'Amazing']} />
+    <StarRating size={24} className="test" defaultRating={3} />
+    <Test />
+  </React.StrictMode>
+)
