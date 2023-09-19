85ef9a2601a1dbf3269ebaa834955ff920cdffef
Alen <info@alenbarac.com>
2023-09-19 07:23:16 +0200

    use state callback fun for setting local storage

--- src/App.jsx
+++ src/App.jsx
@@ -30,12 +30,18 @@ const apiKey = 'a192b306'
 
 export default function App() {
   const [movies, setMovies] = useState([])
-  const [watched, setWatched] = useState([])
+
   const [isOpen2, setIsOpen2] = useState(true)
   const [isLoading, setIsLoading] = useState(false)
   const [errorMsg, setErrorMsg] = useState('')
   const [query, setQuery] = useState('')
   const [selectedId, setSelectedId] = useState(null)
+
+  /*  const [watched, setWatched] = useState([]) */
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem('watched')
+    return JSON.parse(storedValue)
+  })
 
   function handleSelectMovie(id) {
     setSelectedId((current) => (current === id ? null : id))
@@ -47,11 +53,17 @@ export default function App() {
 
   function handleWatchedMovie(movie) {
     setWatched((watched) => [...watched, movie])
+
+    /* localStorage.setItem('watched', JSON.stringify([...watched, movie])) */
   }
 
   function handleRemoveWatchedMovie(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id))
   }
+
+  useEffect(() => {
+    localStorage.setItem('watched', JSON.stringify(watched))
+  }, [watched])
 
   useEffect(() => {
     const controller = new AbortController()
